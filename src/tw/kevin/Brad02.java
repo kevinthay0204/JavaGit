930fc6b5fb68c932b7e4889d6a1544a260f3e91b
Student <Student@Student-PC>
2021-06-28 23:24:54 +0800

    SECOND

---
+++ src/tw/kevin/Brad02.java
@@ -0,0 +1,17 @@
+package tw.kevin;
+
+public class Brad02 {
+
+	public static void main(String[] args) {
+	int a=10, b=3;
+	byte a1=10, b1=3;
+	short a2=10, b2=3;
+	System.out.println(a / b); //byte, short ,int =>
+	System.out.println(a1 / b1);
+	System.out.println(a2 / b2);
+	
+	System.out.println(a % b);
+	
+
+	}
+}
